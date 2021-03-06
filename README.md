aa5e108f2fafdf97227b43ceebd3fce67335bfc9
Akihiro Fuimoto <43336998+AkihiroFujimotoChocolate@users.noreply.github.com>
2021-03-06 19:50:36 +0900

    add IntegratingTextAndGraphics project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SkiaSharpSamples
+
+## HelloSkia
+simple sample 
+
+## IntegratingTextAndGraphics
+The console application version of the sample code in the below url
+https://docs.microsoft.com/xamarin/xamarin-forms/user-interface/graphics/skiasharp/basics/text
---
+++ source/IntegratingTextAndGraphics/Program.cs
@@ -0,0 +1,84 @@
+﻿using SkiaSharp;
+using System;
+using System.IO;
+
+namespace IntegratingTextAndGraphics
+{
+    class Program
+    {
+        const string HelloMessage = "Hello SkiaSharp!";
+        const string OutputFileName = "output.png";
+        const int ImageWidth = 500;
+        const int ImageHeight = 200;
+
+        static void Main(string[] args)
+        {
+            var surface = SKSurface.Create(new SKImageInfo(ImageWidth, ImageHeight));
+            Draw(surface, HelloMessage);
+            using var stream = File.OpenWrite(OutputFileName);
+            Save(stream, surface);
+        }
+
+        // Save surface as a png image file.
+        private static void Save(Stream target, SKSurface surface)
+        {
+            using var image = surface.Snapshot();
+            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+            data.SaveTo(target);
+        }
+
+        // Draw image to surface
+        private static void Draw(SKSurface surface, string message)
+        {
+            // Create a blank canvas
+            var canvas = surface.Canvas;
+            canvas.Clear(SKColors.White);
+
+            // Create an SKPaint object to display the text
+            var textPaint = new SKPaint
+            {
+                Color = SKColors.Chocolate
+            };
+
+            // Find the canvas bounds
+            var canvasBounds = canvas.DeviceClipBounds;
+
+            // Adjust TextSize property so text is 90% of screen width
+            float textWidth = textPaint.MeasureText(message);
+            textPaint.TextSize = 0.9f * canvasBounds.Size.Width * textPaint.TextSize / textWidth;
+
+            // Find the text bounds
+            var textBounds = new SKRect();
+            textPaint.MeasureText(message, ref textBounds);
+
+            // Calculate offsets to center the text on the screen
+            float xText = canvasBounds.MidX - textBounds.MidX;
+            float yText = canvasBounds.MidY - textBounds.MidY;
+
+            // And draw the text
+            canvas.DrawText(message, xText, yText, textPaint);
+
+
+            // Create a new SKRect object for the frame around the text
+            var frameRect = textBounds;
+            frameRect.Offset(xText, yText);
+            frameRect.Inflate(10, 10);
+
+            // Create an SKPaint object to display the frame
+            var framePaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = 5,
+                Color = SKColors.Blue
+            };
+
+            // Draw one frame
+            canvas.DrawRoundRect(frameRect, 20, 20, framePaint);
+
+            // Inflate the frameRect and draw another
+            frameRect.Inflate(10, 10);
+            framePaint.Color = SKColors.DarkBlue;
+            canvas.DrawRoundRect(frameRect, 30, 30, framePaint);
+        }
+    }
+}
