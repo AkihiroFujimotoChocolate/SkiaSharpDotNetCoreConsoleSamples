213a06b49350b88a9797702d0f6cb4f4b154ef4e
Akihiro Fujimoto <akihiro.dvrp01@gmail.com>
2021-03-06 10:53:36 +0900

    first commit (Hello Skia)

---
+++ source/HelloSkia/Program.cs
@@ -0,0 +1,44 @@
+﻿using SkiaSharp;
+using System;
+using System.IO;
+
+namespace HelloSkia
+{
+    class Program
+    {
+        const string  HelloMessage = "Hello Skia!";
+
+        static void Main(string[] args)
+        {
+            // Create an SKSurface object
+            var info = new SKImageInfo(300, 50);
+            using (var surface = SKSurface.Create(info))
+            {
+                // Create a blank SKCanvas
+                var canvas = surface.Canvas;
+                canvas.Clear(SKColors.LightBlue);
+
+                // Draw the message to canvas
+                var paint = new SKPaint
+                {
+                    Color = SKColors.Black,
+                    IsAntialias = true,
+                    Style = SKPaintStyle.Fill,
+                    TextAlign = SKTextAlign.Center,
+                    TextSize = 24
+                };
+                var coord = new SKPoint(info.Width / 2, (info.Height + paint.TextSize) / 2);
+                canvas.DrawText(HelloMessage, coord, paint);
+
+                // Save the SKCanvas as png image file.
+                using (var image = surface.Snapshot())
+                using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+                using (var stream = File.OpenWrite("output.png"))
+                {
+                    data.SaveTo(stream);
+                }
+            }
+
+        }
+    }
+}
